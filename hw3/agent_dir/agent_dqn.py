2b04e92e129a57eaf71c8b3b0111eba9e56d71a5
Lorne_0 <alvin1994alvin@gmail.com>
2017-12-12 23:14:47 +0800

    save 100

--- hw3/agent_dir/agent_dqn.py
+++ hw3/agent_dir/agent_dqn.py
@@ -182,7 +182,7 @@ class Agent_DQN(Agent):
 
             rr = np.mean(result[-100:])
             print("Episode: %d | Reward: %d | Last 100: %f | step: %d | explore: %f" %(e, episode_reward, rr, self.timestep, self.exploration_rate))
-            if (e%10) == 0:
+            if (e%100) == 0:
                 np.save('./result/dqn_keras_result2.npy',result)
                 self.online_model.save('./model/dqn_keras_online_model2.h5')
                 self.target_model.save('./model/dqn_keras_target_model2.h5')

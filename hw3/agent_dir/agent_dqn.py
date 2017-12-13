1aa93532fa4a62b722f507a2f38aa57e207c2153
Lorne_0 <alvin1994alvin@gmail.com>
2017-12-13 11:21:04 +0800

    gamma=0.999

--- hw3/agent_dir/agent_dqn.py
+++ hw3/agent_dir/agent_dqn.py
@@ -16,17 +16,11 @@ K.set_session(get_session())
 
 class Agent_DQN(Agent):
     def __init__(self, env, args):
-        """
-        Initialize every things you need here.
-        For example: building your model
-        """
-
         super(Agent_DQN,self).__init__(env)
-
         if args.test_dqn:
             #you can load your model here
             print('loading trained model')
-            self.online_model = load_model('./model/dqn_keras_online_model.h5')
+            self.online_model = load_model('./model/dqn_keras_online_model_g0.999.h5')
             self.env = env
             self.action_size = self.env.action_space.n
             self.exploration_rate = 1.0
@@ -37,7 +31,7 @@ class Agent_DQN(Agent):
             self.exploration_rate = 1.0
             self.exploration_delta = 9.5*1e-7 # after 1000000, exploration_rate will be 0.05
             self.lr = 1e-4
-            self.gamma = 0.99
+            self.gamma = 0.999
             self.batch_size = 32
             self.timestep = 0
 
@@ -50,33 +44,7 @@ class Agent_DQN(Agent):
             self.online_model = self.build_model()
             self.target_model = self.build_model()
             self.update_target_model()
-            
 
-            #self.optimizer = self.optimizer()
-            #gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.05)
-            #self.sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
-            #K.set_session(self.sess)
-            #self.sess = tf.Session()
-            #self.sess.run(tf.global_variables_initializer())
-
-    '''
-    def optimizer(self):
-        a = K.placeholder(shape=(None,), dtype='int32')
-        y = K.placeholder(shape=(None,), dtype='float32')
-        py_x = self.online_model.output
-        a_one_hot = K.one_hot(a, self.action_size)
-        q_value = K.sum(py_x * a_one_hot, axis=1)
-        error = K.abs(y - q_value)
-        quadratic_part = K.clip(error, 0.0, 1.0)
-        linear_part = error - quadratic_part
-        loss = K.mean(0.5 * K.square(quadratic_part) + linear_part)
-        #opt = RMSprop(lr=self.lr, epsilon=0.01)
-        opt = Adam(lr=self.lr)
-        updates = opt.get_updates(self.online_model.trainable_weights, [], loss)
-        train = K.function([self.online_model.input, a, y], [loss], updates=updates)
-        return train
-    '''
-    
     def build_model(self):
         model = Sequential()
         model.add(Conv2D(32, (8, 8), strides=(4, 4), activation='relu', input_shape=self.state_size, kernel_initializer='he_uniform'))
@@ -183,9 +151,9 @@ class Agent_DQN(Agent):
             rr = np.mean(result[-100:])
             print("Episode: %d | Reward: %d | Last 100: %f | step: %d | explore: %f" %(e, episode_reward, rr, self.timestep, self.exploration_rate))
             if (e%100) == 0:
-                np.save('./result/dqn_keras_result2.npy',result)
-                self.online_model.save('./model/dqn_keras_online_model2.h5')
-                self.target_model.save('./model/dqn_keras_target_model2.h5')
+                np.save('./result/dqn_keras_result_g0.999.npy',result)
+                self.online_model.save('./model/dqn_keras_online_model_0.999.h5')
+                self.target_model.save('./model/dqn_keras_target_model_0.999.h5')
                 #save_path = saver.save(self.sess, "./model/dqn_model03")
 
 
